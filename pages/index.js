41bce982fb09dcc4c7ac3af5eac896d446fdf414
Aiste Tamosauskiene <aistetamosauskiene@gmail.com>
2021-06-15 11:22:38 +0300

    added products from fake store API

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import React, {useState} from "react";
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import products from '../products.json';
 
 export default function Home() {
+
+  const[products, setProducts] = useState([]);
+
+  fetch('https://fakestoreapi.com/products/')
+  .then(response => response.json())
+  .then(products => setProducts(products));
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -14,12 +21,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         E-store
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
+            <strong>Cart:</strong> <span className="snipcart-total-price">&euro;0.00</span>
+          </a>
         </p>
 
         <div className={styles.grid}>
@@ -29,9 +37,15 @@ export default function Home() {
                <img src={product.image} alt={`Preview of ${product.title}`}></img>
                <h3>{product.title}</h3>
                <p>{product.description}</p>
-               <p>{product.price} Eur</p>
+               <p>&euro;{product.price}</p>
                <p>
-                 <button>Add to Cart</button>
+                 <button className="snipcart-add-item"
+                  data-item-id={product.id}
+                  data-item-image={product.image}
+                  data-item-name={product.title}
+                  data-item-url="/"
+                  data-item-price={product.price}
+                 >Add to Cart</button>
                </p>
              </div>
            )
@@ -42,7 +56,7 @@ export default function Home() {
       <footer className={styles.footer}>
       </footer>
       <script async src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js" />
-      <div hidden id="snipcart" data-api-key="MTllZjRkZmYtZmZhYi00MTJlLThjZDgtOThkY2FmZTE3MjY2NjM3NTkzMzY3Mjc4MTc2MjMz" />
+      <div hidden id="snipcart" data-api-key="MTllZjRkZmYtZmZhYi00MTJlLThjZDgtOThkY2FmZTE3MjY2NjM3NTkzMzY3Mjc4MTc2MjMz" data-currency="eur" />
     </div>
   )
 }
